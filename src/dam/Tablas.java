0fc37aa25082fecef1e4b1755cdbb64a5771db45
alemarviz <alemarviz.cma@gmail.com>
2021-02-28 23:20:54 +0100

    Nuevos errores

--- src/dam/Tablas.java
+++ src/dam/Tablas.java
@@ -2,22 +2,22 @@ package dam;
 
 import java.util.Random;
 
-public class Tabla {
+public class Tablas {
 	public static void main(String[] args) {
 		Random rnd = new Random();
 		int[][] tabla = new int[3][6];
 		
 		// Rellenamos la tabla con datos
 		for (int i = 0; i < tabla.length; i++) {
-			for (int j = 0; j < tabla[0].length; j++) {
+			for (int j = 0; j < tabla.length; j++) {
 				tabla[i][j] = rnd.nextInt(10);
 			}
 		}
 		// Mostramos la tabla en pantalla
 		System.out.println("Tabla:");
-		for (int i = 0; i < tabla.length; i++) {
+		for (int i = 0; i < tabla[0].length; i++) {
 			System.out.print("|");
-			for (int j = 0; j < tabla[0].length; j++) {
+			for (int j = 0; j < tabla.length; j++) {
 				System.out.printf("%2d", tabla[i][j]);
 			}
 			System.out.println("|");
